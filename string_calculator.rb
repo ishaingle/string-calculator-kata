5e2bb2eadf411ed0a46b2fa9c42c1af409c1471a
Isha Ingle <ingle.isha@gmail.com>
2025-03-31 21:33:00 +0530

    Added string calculator file

---
+++ string_calculator.rb
@@ -0,0 +1,16 @@
+class StringCalculator
+	def add(numbers)
+		return 0 if numbers.empty?
+
+		delimiter = /,|\n/
+		if numbers.start_with?("//")
+			delimiter = numbers[2]
+			numbers = numbers[4..-1]
+		end
+
+		numbers_list = numbers.split(delimiter).map(&:to_i)
+		negative_numbers = numbers_list.select{|x| x < 0}
+
+		numbers_list.sum
+	end
+end
