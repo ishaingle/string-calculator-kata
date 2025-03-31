e17acfb9e1df91d539b0f29d6425840a6940841f
Isha Ingle <ingle.isha@gmail.com>
2025-03-31 21:33:54 +0530

    Raise error for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -11,6 +11,8 @@ class StringCalculator
 		numbers_list = numbers.split(delimiter).map(&:to_i)
 		negative_numbers = numbers_list.select{|x| x < 0}
 
+		raise CustomError, "negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
+
 		numbers_list.sum
 	end
 end
